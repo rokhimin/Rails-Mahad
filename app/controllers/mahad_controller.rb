b819f6564bc946412989f58330ccaf9c35e51f63
rokhimin <rokhim.whd@gmail.com>
2019-10-22 19:42:53 +0000

    FUNC DISCORD WEBHOOK

--- app/controllers/mahad_controller.rb
+++ app/controllers/mahad_controller.rb
@@ -12,17 +12,32 @@ protect_from_forgery prepend: true
 	def send_discord
 		
 		discord_url = params[:url]
+		discord_username = params[:username]
+		discord_avatar_url = params[:avatar_url]
 		discord_color = params[:color]
 		discord_title = params[:title]
 		discord_url_title = params[:url_title]
 		discord_desc = params[:desc]
+		discord_image = params[:image]
+		discord_thumbnail = params[:thumbnail]
+		discord_author_name = params[:author_name]
+		discord_author_url = params[:author_url]
+		discord_author_icon_url = params[:author_icon_url]
+		discord_footer = params[:footer]
+		discord_icon_footer = params[:icon_footer]
+		discord_field_name = params[:field_name]
+		discord_field_value = params[:field_value]
 		
 		begin
 			discord_url.each do |x|
 			client = Discordrb::Webhooks::Client.new(url: x[1])
 			client.execute do |builder|
-			  builder.username = "anjing"
-			  builder.avatar_url = "https://avatars0.githubusercontent.com/u/25274451?s=460&v=4"
+			discord_username.each do |x|
+			  builder.username = "#{x[1]}"
+			end
+			discord_avatar_url.each do |x|
+			  builder.avatar_url = "#{x[1]}"
+			end
 			  builder.add_embed do |embed|
 				discord_color.each do |x|
 					case x[1]
@@ -57,6 +72,41 @@ protect_from_forgery prepend: true
 					embed.description = "#{x[1]}"
 				end
 				  
+				discord_image.each do |x|
+					embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{x[1]}")
+				end
+				  
+				discord_thumbnail.each do |x|
+ 					embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: "#{x[1]}")
+				end
+				
+				discord_author_url.each do |x|
+					@sigil_author_url = x[1]
+				end
+				discord_author_icon_url.each do |x|
+					@sigil_author_icon_url = x[1]
+				end
+				  
+				discord_author_name.each do |x|
+					embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "#{x[1]}", url: "#{@sigil_author_url}", icon_url: "#{@sigil_author_icon_url}")
+				end
+				  
+				discord_icon_footer.each do |x|
+					@sigil_icon_footer = x[1]
+				end
+					
+				discord_footer.each do |x|
+  					embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "#{x[1]}", icon_url: "#{@sigil_icon_footer}")
+				end
+				  
+				discord_field_value.each do |x|
+					@sigil_field_value = x[1]
+				end
+				  
+				discord_field_name.each do |x|
+				    embed.add_field(name: "#{x[1]}", value: "#{@sigil_field_value}")
+				end
+
 			  end
 			end 
 			end
@@ -64,6 +114,8 @@ protect_from_forgery prepend: true
 			render plain: '{"respond":"failed","code":"0",status:"URI::InvalidURIError"}'
 		rescue RestClient::NotFound
 			render plain: '{"respond":"failed","code":"0",status:"RestClient::NotFound"}'
+		rescue RestClient::BadRequest
+			render plain: '{"respond":"failed","code":"0",status:"RestClient::BadRequest"}'
 		rescue RestClient::Unauthorized
 			render plain: '{"respond":"failed","code":"0",status:"RestClient::Unauthorized"}'
 		rescue SocketError
